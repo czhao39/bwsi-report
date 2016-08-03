3a964ac4c1bcd904fcc1884224bb30dd879ca494
Haicheng Charles Zhao <czhao39@gmail.com>
2016-08-02 21:53:13 -0400

    fade in report images

--- static/index.js
+++ static/index.js
@@ -14,4 +14,7 @@ $document.ready(function() {
     sr.reveal(".section", {duration: 1000});
     sr.reveal("#skills tr", {duration: 1000});
     sr.reveal(".card", {duration: 1000});
+    Materialize.scrollFire([{selector: ".report-img", offset: 100, callback: function(e) {
+        Materialize.fadeInImage($(e));
+    }}]);
 });
